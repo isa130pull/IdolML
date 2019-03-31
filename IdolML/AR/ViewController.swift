f1e12c1cf6eeca8aede184a65ff1d5f2339345d6
i-kawaguchi <isa130pull@gmail.com>
2019-03-31 16:06:19 +0900

    ARでdaybreakを再生する

--- IdolML/AR/ViewController.swift
+++ IdolML/AR/ViewController.swift
@@ -13,21 +13,16 @@ import ARKit
 class ViewController: UIViewController, ARSCNViewDelegate {
 
     @IBOutlet var sceneView: ARSCNView!
+    private var isMute = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         // Set the view's delegate
         sceneView.delegate = self
+        sceneView.debugOptions = .showFeaturePoints
         
-        // Show statistics such as fps and timing information
-        sceneView.showsStatistics = true
-        
-        // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
-        
-        // Set the scene to the view
-        sceneView.scene = scene
+        let gesture = UITapGestureRecognizer(target: self, action: #selector(tapView))
+        sceneView.addGestureRecognizer(gesture)
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -35,8 +30,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Create a session configuration
         let configuration = ARWorldTrackingConfiguration()
-
-        // Run the view's session
+        configuration.planeDetection = .horizontal // 今回追加
         sceneView.session.run(configuration)
     }
     
@@ -46,6 +40,69 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Pause the view's session
         sceneView.session.pause()
     }
+    
+    func createVideoNode(size: CGFloat, position: SCNVector3) -> SCNNode {
+        // AVPlayerを生成する
+        let videoUrl = Bundle.main.url(forResource: "video", withExtension: "mp4")!
+        let avPlayer = AVPlayer(url: videoUrl)
+        avPlayer.actionAtItemEnd = .none
+        avPlayer.isMuted = isMute
+        if !isMute {
+            isMute = true
+        }
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(ViewController.didPlayToEnd),
+                                               name: NSNotification.Name("AVPlayerItemDidPlayToEndTimeNotification"),
+                                               object: avPlayer.currentItem)
+        
+        // SKSceneを生成する
+        let skScene = SKScene(size: CGSize(width: 1000, height: 1000)) // あまりサイズが小さいと、ビデオの解像度が落ちる
+        
+        // AVPlayerからSKVideoNodeの生成する
+        let skNode = SKVideoNode(avPlayer: avPlayer)
+        // シーンと同じサイズとし、中央に配置する
+        skNode.position = CGPoint(x: skScene.size.width / 2.0, y: skScene.size.height / 2.0)
+        skNode.size = skScene.size
+        skNode.yScale = -1.0 // 座標系を上下逆にする
+        skNode.play() // 再生開始
+        
+        // SKSceneに、SKVideoNodeを追加する
+        skScene.addChild(skNode)
+        
+        // SCNBoxノードを生成して、マテリアルにSKSeceを適用する
+        let node = SCNNode()
+        node.geometry = SCNBox(width: size, height: size, length: size, chamferRadius: 0)
+        let material = SCNMaterial()
+        material.diffuse.contents = skScene
+        node.geometry?.materials = [material]
+        node.scale = SCNVector3(1.7, 1, 1) // サイズは横長
+        node.position = position
+
+        return node
+    }
+    
+    @objc func didPlayToEnd(notification: NSNotification) {
+        let item: AVPlayerItem = notification.object as! AVPlayerItem
+        item.seek(to: .zero, completionHandler: nil)
+    }
+    
+    @objc func tapView(sender: UITapGestureRecognizer) {
+        let location = sender.location(in: sceneView)
+        
+        let hitTestResult = sceneView.hitTest(location, types: .existingPlane)
+        if let result = hitTestResult.first {
+//            let geometry = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0)
+//            let material = SCNMaterial()
+//            material.diffuse.contents = UIColor.darkGray
+//            geometry.materials = [material]
+//
+//            let node = SCNNode(geometry: geometry)
+            let pos = SCNVector3(result.worldTransform.columns.3.x, result.worldTransform.columns.3.y + 1, result.worldTransform.columns.3.z)
+            let node = createVideoNode(size: 1.0, position: pos)
+            sceneView.scene.rootNode.addChildNode(node)
+        }
+    }
+    
 
     // MARK: - ARSCNViewDelegate
     
